14e39f5f5fca83e9c36a30fd18f93e417cd3825b
scytherswings <scytherswings@gmail.com>
2018-04-29 12:25:44 -0400

    rubocop fix

--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,7 @@ Rails.application.routes.draw do
       resources :pin_assortments
       resources :users
       match 'me' => 'me#show', via: :get
-      match 'me' => 'me#update', via: [:patch, :put]
+      match 'me' => 'me#update', via: %i[patch put]
     end
 
     namespace :v1 do
