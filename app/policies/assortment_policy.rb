d63d1daec5b65cdc10cbab66017ce278c8aa655b
scytherswings <scytherswings@gmail.com>
2018-04-29 16:11:21 -0400

    assortment, collection and pin policies are almost finished

--- app/policies/assortment_policy.rb
+++ app/policies/assortment_policy.rb
@@ -27,17 +27,4 @@ class AssortmentPolicy < ApplicationPolicy
   def destroy?
     user&.moderator?
   end
-
-  class Scope
-    attr_reader :user, :scope
-
-    def initialize(user, scope)
-      @user = user
-      @scope = scope
-    end
-
-    def resolve
-      scope
-    end
-  end
 end
