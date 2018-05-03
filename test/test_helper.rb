b2b0264bfc8c273041c7aee21cb5e1b3963c8ad8
scytherswings <scytherswings@gmail.com>
2018-05-02 20:16:12 -0400

    gitignore files

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,6 +5,15 @@ require File.expand_path('../config/environment', __dir__)
 require 'rails/test_help'
 require 'policy_assertions'
 require 'test_helpers/policy_test_helper'
+require 'simplecov'
+
+# save to CircleCI's artifacts directory if we're on CircleCI
+if ENV['CIRCLE_ARTIFACTS']
+  dir = File.join(ENV['CIRCLE_ARTIFACTS'], "coverage")
+  SimpleCov.coverage_dir(dir)
+end
+
+SimpleCov.start
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
