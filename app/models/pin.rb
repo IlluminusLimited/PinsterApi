170fef303cf1985ca3392b028e015e0922acb7a0
scytherswings <scytherswings@gmail.com>
2018-05-16 01:40:33 -0400

    simplify sql

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -26,30 +26,12 @@ class Pin < ApplicationRecord
 
   def images
     Image.find_by_sql <<~SQL
-                select images.id as id,
-        images.description,
-        imageable_type,
-        imageable_id,
-        images.name,
-        base_file_name,
-        storage_location_uri,
-        images.created_at,
-        images.updated_at,
-        featured
+      select images.*
       from images
         inner join pins on imageable_id = pins.id
       where imageable_id = '#{id}' and imageable_type = 'Pin'
       union
-      select  images.id as id,
-        images.description,
-        imageable_type,
-        imageable_id,
-        images.name,
-        base_file_name,
-        storage_location_uri,
-        images.created_at,
-        images.updated_at,
-        featured
+      select images.*
       from images
         inner join assortments on imageable_id = assortments.id and imageable_type = 'Assortment'
         inner join pin_assortments on pin_assortments.assortment_id = assortments.id
