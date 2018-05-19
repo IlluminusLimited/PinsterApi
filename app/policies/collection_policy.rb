62b1e4c88b27bd5e7b34176126f5cc071a6fb653
Andrew Newell <scytherswings@users.noreply.github.com>
2018-05-19 15:52:33 -0400

    fixes #57 (#58)
    * fixes #57
    
    * safe navigation not necessary because null object pattern is amazing

--- app/policies/collection_policy.rb
+++ app/policies/collection_policy.rb
@@ -39,16 +39,17 @@ class CollectionPolicy < ApplicationPolicy
   end
 
   class Scope < Scope
-    attr_reader :user, :scope
+    attr_reader :current_user, :collection_user_id, :scope
 
-    def initialize(user, scope)
-      @user = user
+    def initialize(current_user, collection_user_id, scope)
+      @current_user = current_user
+      @collection_user_id = collection_user_id
       @scope = scope
     end
 
     def resolve
-      return scope.all if user.admin?
-      scope.where(user_id: user.id, public: true)
+      return scope.where(user_id: collection_user_id) if current_user.admin? || current_user.id == collection_user_id
+      scope.where(user_id: collection_user_id, public: true)
     end
   end
 end
