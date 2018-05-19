5ef3c27d1965586fbd3b1d0c3d9506fd071413f3
scytherswings <scytherswings@gmail.com>
2018-05-19 02:50:24 -0400

    don't require login to view users collections

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,7 @@ require 'rails/test_unit/railtie'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-if Rails.env.development?
+if Rails.env.development? || ENV['REMOTE_DEBUG']
   require 'dotenv'
   Dotenv.load
 end
