776dd71a4df3386dad73e733e2dea9fe87719f9e
scytherswings <scytherswings@gmail.com>
2018-05-21 03:27:17 -0400

    idk

--- app/controllers/oauths_controller.rb
+++ app/controllers/oauths_controller.rb
@@ -6,17 +6,20 @@ class OauthsController < ApplicationController
   after_action :verify_authorized, except: %i[login oauth callback]
 
   api :GET, '/login', 'Get list of oauth providers'
+
   def login; end
 
   # :nocov:
   api :GET, '/login/:provider', "Redirect to Oauth provider's flow"
   param :provider, String, desc: 'Oauth provider', required: true
+
   def oauth
     login_at(auth_params[:provider])
   end
 
   api :GET, '/oauth/callback', 'Callback url for Oauth provider'
   param :provider, String, desc: 'Oauth provider'
+
   def callback
     provider = auth_params[:provider]
 
@@ -24,7 +27,7 @@ class OauthsController < ApplicationController
     user ||= auth_and_login(provider)
 
     token = user.authentications.find_by(provider: provider).refresh_token
-    redirect_to(ENV['CLIENT_AUTH_CALLBACK_URL'] || root_url, {token: token})
+    redirect_to((ENV['CLIENT_AUTH_CALLBACK_URL'] || root_url) + '?token=' + token)
   end
 
   private
