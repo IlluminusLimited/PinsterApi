087f500f014adf3ebaa9e268772bd39e21ab9358
scytherswings <scytherswings@gmail.com>
2019-05-05 13:41:58 -0400

    fix httplog call

--- config/initializers/httplog.rb
+++ config/initializers/httplog.rb
@@ -1,34 +1,36 @@
 # frozen_string_literal: true
 
-HttpLog.configure do |config|
-  # Enable or disable all logging
-  config.enabled = Rails.env.development?
+if Rails.env.development?
+  HttpLog.configure do |config|
+    # Enable or disable all logging
+    config.enabled = Rails.env.development?
 
-  # You can assign a different logger
-  config.logger = Logger.new($stdout)
+    # You can assign a different logger
+    config.logger = Logger.new($stdout)
 
-  # I really wouldn't change this...
-  config.severity = Logger::Severity::DEBUG
+    # I really wouldn't change this...
+    config.severity = Logger::Severity::DEBUG
 
-  # Tweak which parts of the HTTP cycle to log...
-  config.log_connect   = true
-  config.log_request   = true
-  config.log_headers   = false
-  config.log_data      = true
-  config.log_status    = true
-  config.log_response  = true
-  config.log_benchmark = true
+    # Tweak which parts of the HTTP cycle to log...
+    config.log_connect = true
+    config.log_request = true
+    config.log_headers = false
+    config.log_data = true
+    config.log_status = true
+    config.log_response = true
+    config.log_benchmark = true
 
-  # ...or log all request as a single line by setting this to `true`
-  config.compact_log = false
+    # ...or log all request as a single line by setting this to `true`
+    config.compact_log = false
 
-  # You can also log in JSON format
-  config.json_log = true
+    # You can also log in JSON format
+    config.json_log = true
 
-  # Prettify the output - see below
-  config.color = false
+    # Prettify the output - see below
+    config.color = false
 
-  # Limit logging based on URL patterns
-  config.url_whitelist_pattern = nil
-  config.url_blacklist_pattern = nil
+    # Limit logging based on URL patterns
+    config.url_whitelist_pattern = nil
+    config.url_blacklist_pattern = nil
+  end
 end
