e55274ee8816023b3ded1816883dc1e6962eaf7c
scytherswings <scytherswings@gmail.com>
2019-05-10 22:07:55 -0400

    always use 32 characters for testing

--- app/controllers/tools_controller.rb
+++ app/controllers/tools_controller.rb
@@ -10,7 +10,7 @@ class ToolsController < ApplicationController
   show false
 
   def crypto_codes
-    raw_verifier = SecureRandom.base64(43)
+    raw_verifier = "x" * 32
     code_verifier = Base64.urlsafe_encode64(raw_verifier, padding: false)
     code_challenge = Base64.urlsafe_encode64(Digest::SHA256.hexdigest(code_verifier), padding: false)
 
