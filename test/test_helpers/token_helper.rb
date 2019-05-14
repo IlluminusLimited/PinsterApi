050b9415d3e2adb42612e8d11c95975844efa15d
scytherswings <scytherswings@gmail.com>
2019-05-14 19:35:40 -0400

    added auth0 token factory test

--- test/test_helpers/token_helper.rb
+++ test/test_helpers/token_helper.rb
@@ -8,14 +8,16 @@ class TokenHelper
   PUBLIC_KEY = PRIVATE_KEY.public_key
 
   def resolver
-    Utilities::TokenFactoryResolver.new(decoders: { ISS => Utilities::Auth0TokenFactory.new(jwt_decoder: jwt_decoder) })
+    Utilities::TokenFactoryResolver.new(decoders: {
+                                          ISS => Utilities::Auth0TokenFactory.new(jwt_decoder: TokenHelper.jwt_decoder)
+                                        })
   end
 
   def self.for_user(user, permissions = [], exp = (Time.now.in_time_zone + 20.minutes).to_i)
     generate_token(user.external_user_id, permissions, exp)
   end
 
-  def self.generate_token(sub, permissions, exp)
+  def self.generate_token(sub, permissions = [], exp = 1.minute.from_now.to_i)
     payload = {
       "iss": ISS,
       "sub": sub,
@@ -29,18 +31,16 @@ class TokenHelper
     JWT.encode payload, PRIVATE_KEY, 'RS256'
   end
 
-  private
-
-    def jwt_decoder
-      lambda do |jwt|
-        JWT.decode(jwt,
-                   TokenHelper::PUBLIC_KEY,
-                   true, # Verify the signature of this token
-                   algorithm: 'RS256',
-                   iss: TokenHelper::ISS,
-                   verify_iss: true,
-                   aud: TokenHelper::AUD,
-                   verify_aud: true)
-      end
+  def self.jwt_decoder
+    lambda do |jwt|
+      JWT.decode(jwt,
+                 TokenHelper::PUBLIC_KEY,
+                 true, # Verify the signature of this token
+                 algorithm: 'RS256',
+                 iss: TokenHelper::ISS,
+                 verify_iss: true,
+                 aud: TokenHelper::AUD,
+                 verify_aud: true)
     end
+  end
 end
