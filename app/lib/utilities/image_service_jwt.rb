b6214b2e4a36ec4b70c5a3086bf503849a1c40f4
scytherswings <scytherswings@gmail.com>
2019-05-14 18:23:55 -0400

    logging for keys

--- app/lib/utilities/image_service_jwt.rb
+++ app/lib/utilities/image_service_jwt.rb
@@ -7,9 +7,13 @@ module Utilities
     attr_reader :iss, :aud, :key
 
     def initialize(opts = {})
-      @key = opts[:key] ||= OpenSSL::PKey::RSA.new(Base64.urlsafe_decode64(ENV['PRIVATE_KEY']))
-      @image_service_public_key = opts[:image_service_public_key] ||=
-                                    OpenSSL::PKey::RSA.new(Base64.urlsafe_decode64(ENV['IMAGE_SERVICE_PUBLIC_KEY']))
+
+      private_key = Base64.urlsafe_decode64(ENV['PRIVATE_KEY'])
+      image_service_key = Base64.urlsafe_decode64(ENV['IMAGE_SERVICE_PUBLIC_KEY']))
+      logger.debug { "Private key: #{private_key.to_s}" }
+      logger.debug("Image service public key: #{image_service_key}")
+      @key = opts[:key] ||= OpenSSL::PKey::RSA.new(private_key)
+      @image_service_public_key = opts[:image_service_public_key] ||= OpenSSL::PKey::RSA.new(image_service_key)
       @iss = opts[:iss] ||= ENV['JWT_AUD'] # Our JWT_AUD is our own uri, so when we encode tokens it's our ISS
       @aud = opts[:aud] ||= ENV['IMAGE_SERVICE_URL'] # The AUD of image service tokens is image service's URL
     end
@@ -38,9 +42,9 @@ module Utilities
 
     def testing_token
       metadata = {
-        imageable_id: "74bff7c0-529d-41a4-a1bb-f04fe182929c",
-        imageable_type: "Pin",
-        user_id: "1771dd50-2485-4569-af3a-c3a0849c5e32"
+          imageable_id: "74bff7c0-529d-41a4-a1bb-f04fe182929c",
+          imageable_type: "Pin",
+          user_id: "1771dd50-2485-4569-af3a-c3a0849c5e32"
       }
       generate_token(metadata, exp: 8.hours.from_now.to_i)
     end
