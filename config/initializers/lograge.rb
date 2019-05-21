4932e725946f8fb70a16622b003077e3b66c3ec0
scytherswings <scytherswings@gmail.com>
2019-05-20 23:38:41 -0500

    hurp durp

--- config/initializers/lograge.rb
+++ config/initializers/lograge.rb
@@ -1,14 +1,11 @@
-if Rails.env.production?
-  Rails.application.configure do
-    config.lograge.enabled = true
-    config.lograge.base_controller_class = 'ActionController::API'
+Rails.application.configure do
+  config.lograge.base_controller_class = 'ActionController::API'
 
-    config.lograge.ignore_actions = ["HealthCheck::HealthCheckController#index"]
-    config.lograge.custom_options = lambda do |event|
-      exceptions = %w(controller action format id)
-      {
-          params: event.payload[:params].except(*exceptions)
-      }
-    end
+  config.lograge.ignore_actions = ["HealthCheck::HealthCheckController#index"]
+  config.lograge.custom_options = lambda do |event|
+    exceptions = %w(controller action format id)
+    {
+        params: event.payload[:params].except(*exceptions)
+    }
   end
 end
