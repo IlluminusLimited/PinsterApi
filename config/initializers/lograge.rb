059be11c33708c37cf74cedf3e06905fce70f89b
scytherswings <scytherswings@gmail.com>
2019-05-20 22:51:34 -0500

    tweak it so we get params

--- config/initializers/lograge.rb
+++ config/initializers/lograge.rb
@@ -4,5 +4,11 @@ if Rails.env.production?
     config.lograge.base_controller_class = 'ActionController::API'
 
     config.lograge.ignore_actions = ["HealthCheck::HealthCheckController#index"]
+    config.lograge.custom_options = lambda do |event|
+      exceptions = %w(controller action format id)
+      {
+          params: event.payload[:params].except(*exceptions)
+      }
+    end
   end
 end
