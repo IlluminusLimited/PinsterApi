c465f7b732a1316416009147f856ec6e7f988c39
scytherswings <scytherswings@gmail.com>
2019-05-22 21:18:18 -0500

    config papertrail fixes #130

--- config/initializers/paper_trail.rb
+++ config/initializers/paper_trail.rb
@@ -1,3 +1,8 @@
 # frozen_string_literal: true
 
 PaperTrail.serializer = PaperTrail::Serializers::JSON
+PaperTrail.config.enabled = true
+PaperTrail.config.has_paper_trail_defaults = {
+  on: %i[create update destroy]
+}
+PaperTrail.config.version_limit = 3
